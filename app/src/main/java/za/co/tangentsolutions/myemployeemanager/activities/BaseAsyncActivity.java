f8b61d57f386f01875140ca7ba951aa5a2e43fb5
Tshepo <rocboyt2gmail.com>
2018-03-18 19:35:38 +0200

    Fixed layout built errors

--- app/src/main/java/za/co/tangentsolutions/myemployeemanager/activities/BaseAsyncActivity.java
+++ app/src/main/java/za/co/tangentsolutions/myemployeemanager/activities/BaseAsyncActivity.java
@@ -1,15 +1,43 @@
 package za.co.tangentsolutions.myemployeemanager.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import za.co.tangentsolutions.myemployeemanager.R;
+import za.co.tangentsolutions.myemployeemanager.fragments.LoadingSpinnerFragment;
+import za.co.tangentsolutions.myemployeemanager.views.BaseAsyncView;
 
-public class BaseAsyncActivity extends AppCompatActivity {
+public abstract class BaseAsyncActivity extends BaseActivity implements BaseAsyncView {
+
+    public void showLoadingDialogFragment(String loadingMessage) {
+        showFragmentDialog(loadingMessage, R.layout.loading_spinner, new LoadingSpinnerFragment());
+    }
+
+    public void hideLoadingDialogFragment() {
+        if(dialogFragment != null)
+            dialogFragment.dismiss();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base_async);
+    public void onBackPressed() {
+        super.onBackPressed();
+        presenter.handleBackButtonPressed();
     }
+
+    @Override
+    public void showLoadingDialog(String loadingMessage) {
+        showLoadingDialogFragment(loadingMessage);
+    }
+
+    @Override
+    public void hideLoadingDialog() {
+        hideLoadingDialogFragment();
+    }
+
+    @Override
+    public void showHttpCallError(String errorMessage) {
+
+        if(errorMessage.isEmpty())
+            errorMessage = getString(R.string.generic_error_message);
+
+        showAlertDialogErrorMessage(getString(R.string.error_alert_title), errorMessage);
+    }
+
 }
