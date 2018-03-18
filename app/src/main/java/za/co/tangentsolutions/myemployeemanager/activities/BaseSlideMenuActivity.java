585d61f9399f261a6306daba6f77e0fb150861e9
Tshepo <rocboyt2gmail.com>
2018-03-18 16:20:19 +0200

    Added app activities

---
+++ app/src/main/java/za/co/tangentsolutions/myemployeemanager/activities/BaseSlideMenuActivity.java
@@ -0,0 +1,8 @@
+package za.co.tangentsolutions.myemployeemanager.activities;
+
+/**
+ * Created by Tshepo on 3/18/2018.
+ */
+
+public class BaseSlideMenuActivity {
+}
---
+++ app/src/main/java/za/co/tangentsolutions/myemployeemanager/activities/EmloyeeStatsActivity.java
@@ -0,0 +1,12 @@
+package za.co.tangentsolutions.myemployeemanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmloyeeStatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
