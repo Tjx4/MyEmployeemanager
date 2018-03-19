7adc8883c1b17b92371edc495aadf29d0682e5ba
Tshepo <rocboyt2gmail.com>
2018-03-19 07:57:34 +0200

    Added stats listView

--- app/src/main/java/za/co/tangentsolutions/myemployeemanager/activities/EmloyeeStatsActivity.java
+++ app/src/main/java/za/co/tangentsolutions/myemployeemanager/activities/EmloyeeStatsActivity.java
@@ -1,14 +1,33 @@
 package za.co.tangentsolutions.myemployeemanager.activities;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.List;
 import za.co.tangentsolutions.myemployeemanager.R;
+import za.co.tangentsolutions.myemployeemanager.adapters.EmployeeDetailsAdapter;
+import za.co.tangentsolutions.myemployeemanager.adapters.EmployeeStatsAdapter;
+import za.co.tangentsolutions.myemployeemanager.models.EmployeeStatModel;
+import za.co.tangentsolutions.myemployeemanager.presenters.EmployeeStatsPrestenter;
+import za.co.tangentsolutions.myemployeemanager.views.EmployeeStatsView;
 
-public class EmloyeeStatsActivity extends BaseChildActivity {
+public class EmloyeeStatsActivity extends BaseChildActivity implements EmployeeStatsView {
+
+    private ListView employeeStatLstV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emloyee_stats);
         currentActionBar.setTitle(R.string.employee_stats);
+        employeeStatLstV = findViewById(R.id.lstVEmployeeStat);
+        setPresenter(new EmployeeStatsPrestenter(this));
+    }
+
+    @Override
+    public void showEmployeeDetails(List<EmployeeStatModel> employeeDetailsList) {
+        EmployeeStatsAdapter employeeStatAdapter = new EmployeeStatsAdapter(this, R.layout.employee_stat_view, employeeDetailsList);
+
+        employeeStatLstV = findViewById(R.id.lstvEmployeeInfo);
+        employeeStatLstV.setAdapter(employeeStatAdapter);
     }
 }
--- app/src/main/java/za/co/tangentsolutions/myemployeemanager/views/EmployeeStatsView.java
+++ app/src/main/java/za/co/tangentsolutions/myemployeemanager/views/EmployeeStatsView.java
@@ -1,4 +1,8 @@
 package za.co.tangentsolutions.myemployeemanager.views;
 
+import java.util.List;
+import za.co.tangentsolutions.myemployeemanager.models.EmployeeStatModel;
+
 public interface EmployeeStatsView extends BaseAsyncView {
+    void showEmployeeDetails(List<EmployeeStatModel> employeeDetailsList);
 }
